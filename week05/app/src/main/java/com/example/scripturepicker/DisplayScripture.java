b99ffd41375a1e3bf5a8698b769c72199ae08fc1
caleb <caleb@byui.edu>
2019-10-14 17:15:40 -0600

    Adding week 05 code submission with part 1 done

---
+++ week05/app/src/main/java/com/example/scripturepicker/DisplayScripture.java
@@ -0,0 +1,4 @@
+package com.example.scripturepicker;
+
+class DisplayScripture {
+}
---
+++ week05/app/src/main/java/com/example/scripturepicker/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.scripturepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String book;
+    int chapter;
+    int verse;
+
+    String scripture;
+
+    EditText bookInput;
+    EditText chapterInput;
+    EditText verseInput;
+
+    Button submitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bookInput = (EditText) findViewById(R.id.book);
+        chapterInput = (EditText) findViewById(R.id.chapter);
+        verseInput = (EditText) findViewById(R.id.verse);
+        submitButton = (Button) findViewById(R.id.submitButton);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                book = bookInput.getText().toString();
+                chapter = Integer.valueOf(chapterInput.getText().toString());
+                verse = Integer.valueOf(verseInput.getText().toString());
+
+                scripture = book + " " + chapter + ":" + verse;
+
+                displayScripture();
+            }
+        });
+    }
+
+    private void displayScripture() {
+        Intent intent = new Intent(this,DisplayScripture.class);
+        intent.putExtra(scripture);
+    }
+}
